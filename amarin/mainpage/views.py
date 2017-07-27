92c70ddbd84f0553ef724a37ba65928705e64df2
Raul Nunez <rnunez2@ucmerced.edu>
2017-07-27 00:39:35 -0700

    Now loads menu from db

--- amarin/mainpage/views.py
+++ amarin/mainpage/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import MenuItem
 # Create your views here.
 
 def index(request):
     return render(request, 'mainpage/index.html')
 
 def menu(request):
-    return render(request, 'mainpage/LunchMenu.html')
+    fullMenu = MenuItem.objects.all()
+    context = {
+        'menu': fullMenu
+    }
+    return render(request, 'mainpage/LunchMenu.html', context)
